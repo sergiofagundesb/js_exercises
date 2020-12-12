d60fa6d78ed33cb8eb43b1f5c9fe1f60dbf5c949
sergiofagundesb <sergio.fagundesbarros@gmail.com>
2020-12-12 19:48:53 -0300

    add exercicios aula 15

---
+++ lampada.js
@@ -0,0 +1,20 @@
+<script>
+var lampadaPlugada;
+var bulboQueimado;
+
+if(lampadaPlugada == true)
+{
+    if(bulboQueimado == true)
+    {
+        console.log("Trocar o bulbo");
+    }
+    else
+    {
+        console.log("Comprar uma lâmpada nova");
+    }
+}
+else
+{
+    console.log("Plugar a lâmpada");
+}
+</script>
